70b2fba617470215c31d6639c193c177c85acc4d
alan-menezes <alan_menezzes@hotmail.com>
2023-10-03 00:55:33 -0300

    adicionei os idiomas ingles e portugues no projeto, agora  o mesmo texto pode ser exibido e escutados em 3 idiomas,espanhol(padrao) portugues e ingles.Tambem adicionei a imagem de cada noticia

--- main.js
+++ main.js
@@ -1,28 +1,129 @@
 // Objeto de textos em espanhol
 const textosEspanhol = {
-    "Quién es el “Niño Guerrero”, el líder del Tren de Aragua que controlaba la cárcel de Tocorón": "Es catalogado como uno de los hombres más peligrosos de Venezuela Héctor Guerrero Flores, más conocido como el “Niño Guerrero”, es el cabecilla del Tren de Aragua, el mayor grupo de crimen organizado del país y uno de los más importantes de América Latina. Su centro de operaciones era la cárcel de Tocorón, ubicada a unos 140 kilómetros al suroeste de Caracas, que este miércoles fue intervenida por miles de policías y militares con el fin de desarticular a las bandas criminales.Las autoridades venezolanas aseguraron que lograron tomar el control total de la prisión, sin embargo, no precisaron qué ocurrió con los líderes de la organización criminal.",
-    "Cómo reconfiguré mi cerebro en 6 semanas": "Cada vez hay más pruebas de que cambios simples y cotidianos en nuestra vida pueden alterar nuestro cerebro y cambiar su funcionamiento. Melissa Hogenboom, periodista de la BBC, sometió su propio cerebro a un escaneo para averiguarlo.'Es sorprendentemente difícil no pensar en nada en absoluto', pienso mientras estoy recostada dentro de las fauces de una máquina que escanea mi cerebro. Me dicen que me concentre en una cruz negra mientras la máquina de imágenes de resonancia magnética funcional (IRMf) hace su ruidoso trabajo. También me resulta imposible mantener los ojos abiertos. El zumbido del escáner es algo hipnótico y me preocupa un poco quedarme dormida y que esto afecte cómo aparezca mi cerebro en las imágenes resultantes.",
-    "La conmovedora lucha del músico Luciano Supervielle para que su hija con acondroplasia reciba medicación en Uruguay": "Giras internacionales, conciertos en Europa, proyectos como solista: la carrera del músico uruguayo Luciano Supervielle iba en pleno ascenso cuando a su esposa le diagnosticaron cáncer. Y todo cambió.Supervielle interrumpió su vida profesional para darle prioridad a la salud de Eloísa, cuyo fallecimiento dos años después en 2020 aún recuerda conmovido, y al cuidado de sus dos hijos que viven con él en Montevideo.Esto ha implicado difíciles retos: Julián, de 11 años, tiene síndrome de Down, y Nina, de 9, padece acondroplasia.",
-    "La maestra que cada día recorre 200 kilómetros haciendo autostop para dar clase a dos niños en medio del campo en Uruguay": "Al borde de la carretera, donde comienza la ruta 56, con su bata blanca puesta para que identifiquen que es maestra, estira el brazo derecho y muestra su mano. Son las 8 de una gélida mañana de invierno y María Domínguez (29 años) está en la entrada de la pequeña ciudad de Florida, 90 kilómetros al norte de Montevideo, intentando que algún chofer se detenga y le ofrezca un aventón. Tiene que estar antes de las 10 en la escuela rural de Paso de la Cruz del Yí, a 108 kilómetros de su casa, en medio de la nada, para darles clases a Juliana, de 4 años, y a Benjamín, de 9, los únicos dos alumnos de ese centro educativo uruguayo."
+    "Quién es el “Niño Guerrero”, el líder del Tren de Aragua que controlaba la cárcel de Tocorón": {
+        texto: "Es catalogado como uno de los hombres más peligrosos de Venezuela Héctor Guerrero Flores, más conocido como el “Niño Guerrero”, es el cabecilla del Tren de Aragua, el mayor grupo de crimen organizado del país y uno de los más importantes de América Latina. Su centro de operaciones era la cárcel de Tocorón, ubicada a unos 140 kilómetros al suroeste de Caracas, que este miércoles fue intervenida por miles de policías y militares con el fin de desarticular a las bandas criminales.Las autoridades venezolanas aseguraron que lograron tomar el control total de la prisión, sin embargo, no precisaron qué ocurrió con los líderes de la organización criminal.",
+        imagem: "https://ichef.bbci.co.uk/news/800/cpsprodpb/7339/live/91d730e0-5932-11ee-86c3-87504a353e41.jpg"
+    },
+    "Cómo reconfiguré mi cerebro en 6 semanas": {
+        texto: "Cada vez hay más pruebas de que cambios simples y cotidianos en nuestra vida pueden alterar nuestro cerebro y cambiar su funcionamiento. Melissa Hogenboom, periodista de la BBC, sometió su propio cerebro a un escaneo para averiguarlo.'Es sorprendentemente difícil no pensar en nada en absoluto', pienso mientras estoy recostada dentro de las fauces de una máquina que escanea mi cerebro. Me dicen que me concentre en una cruz negra mientras la máquina de imágenes de resonancia magnética funcional (IRMf) hace su ruidoso trabajo. También me resulta imposible mantener los ojos abiertos. El zumbido del escáner es algo hipnótico y me preocupa un poco quedarme dormida y que esto afecte cómo aparezca mi cerebro en las imágenes resultantes.",
+        imagem: "https://ichef.bbci.co.uk/news/800/cpsprodpb/3942/live/00fd73c0-5704-11ee-a657-3d3f9f088e1b.jpg"
+    },
+    "La conmovedora lucha del músico Luciano Supervielle para que su hija con acondroplasia reciba medicación en Uruguay": {
+        texto: "Giras internacionales, conciertos en Europa, proyectos como solista: la carrera del músico uruguayo Luciano Supervielle iba en pleno ascenso cuando a su esposa le diagnosticaron cáncer. Y todo cambió.Supervielle interrumpió su vida profesional para darle prioridad a la salud de Eloísa, cuyo fallecimiento dos años después en 2020 aún recuerda conmovido, y al cuidado de sus dos hijos que viven con él en Montevideo.Esto ha implicado difíciles retos: Julián, de 11 años, tiene síndrome de Down, y Nina, de 9, padece acondroplasia.",
+        imagem: "https://www.revolvermag.com/sites/default/files/media/section-media/marioduplantier_credit_miikka_skaffarifilmmagic.jpg"
+    },
+    "La maestra que cada día recorre 200 kilómetros haciendo autostop para dar clase a dos niños en medio del campo en Uruguay": {
+        texto: "Al borde de la carretera, donde comienza la ruta 56, con su bata blanca puesta para que identifiquen que es maestra, estira el brazo derecho y muestra su mano. Son las 8 de una gélida mañana de invierno y María Domínguez (29 años) está en la entrada de la pequeña ciudad de Florida, 90 kilómetros al norte de Montevideo, intentando que algún chofer se detenga y le ofrezca un aventón. Tiene que estar antes de las 10 en la escuela rural de Paso de la Cruz del Yí, a 108 kilómetros de su casa, en medio de la nada, para darles clases a Juliana, de 4 años, y a Benjamín, de 9, los únicos dos alumnos de ese centro educativo uruguayo.",
+        imagem: "https://www.noticias24hrs.com.ve/wp-content/uploads/2023/09/Screenshot_20230913-222243.jpg"
+    }
     // Adicione mais textos conforme necessário
 };
+// Objeto de textos em ingles
+const textosIngles = {
+    "Who is the 'Niño Guerrero,' the leader of the Tren de Aragua that controlled Tocorón prison": {
+        texto: "He is classified as one of the most dangerous men in Venezuela. Héctor Guerrero Flores, better known as the “Niño Guerrero”, is the leader of the Tren de Aragua, the largest organized crime group in the country and one of the most important in Latin America. His operations center was the Tocorón prison, located about 140 kilometers southwest of Caracas, which was intervened by thousands of police and military personnel this Wednesday in order to dismantle the criminal gangs. Venezuelan authorities stated that they managed to take full control of the prison; however, they did not specify what happened to the leaders of the criminal organization.",
+        imagem: "https://ichef.bbci.co.uk/news/800/cpsprodpb/7339/live/91d730e0-5932-11ee-86c3-87504a353e41.jpg"
+    },
+    "How I Rewired My Brain in 6 Weeks": {
+        texto: "There is increasing evidence that simple and everyday changes in our lives can alter our brains and change their functioning. Melissa Hogenboom, a BBC journalist, subjected her own brain to a scan to find out. 'It's surprisingly difficult not to think of anything at all,' I think as I lie inside the jaws of a brain scanning machine. I'm told to focus on a black cross while the functional magnetic resonance imaging (fMRI) machine does its noisy work. I also find it impossible to keep my eyes open. The hum of the scanner is somewhat hypnotic, and I'm a little worried about falling asleep and how this might affect how my brain appears in the resulting images.",
+        imagem: "https://ichef.bbci.co.uk/news/800/cpsprodpb/3942/live/00fd73c0-5704-11ee-a657-3d3f9f088e1b.jpg"
+    },
+    "The Heartwarming Struggle of Musician Luciano Supervielle for His Daughter with Achondroplasia to Receive Medication in Uruguay": {
+        texto: "Giras internacionales, conciertos en Europa, proyectos como solista: la carrera del músico uruguayo Luciano Supervielle iba en pleno ascenso cuando a su esposa le diagnosticaron cáncer. Y todo cambió.Supervielle interrumpió su vida profesional para darle prioridad a la salud de Eloísa, cuyo fallecimiento dos años después en 2020 aún recuerda conmovido, y al cuidado de sus dos hijos que viven con él en Montevideo.Esto ha implicado difíciles retos: Julián, de 11 años, tiene síndrome de Down, y Nina, de 9, padece acondroplasia.",
+        imagem: "https://www.revolvermag.com/sites/default/files/media/section-media/marioduplantier_credit_miikka_skaffarifilmmagic.jpg"
+    },
+    "The Teacher Who Hitchhikes 200 Kilometers Every Day to Teach Two Children in the Middle of the Field in Uruguay": {
+        texto: "At the edge of the road, where route 56 begins, with her white coat on so they know she's a teacher, she stretches out her right arm and shows her hand. It's 8 o'clock on a freezing winter morning, and María Domínguez (29 years old) is at the entrance to the small town of Florida, 90 kilometers north of Montevideo, trying to get a driver to stop and give her a ride. She has to be at the rural school of Paso de la Cruz del Yí, 108 kilometers from her home, in the middle of nowhere, to teach Juliana, 4, and Benjamín, 9, the only two students at that Uruguayan educational center.",
+        imagem: "https://www.noticias24hrs.com.ve/wp-content/uploads/2023/09/Screenshot_20230913-222243.jpg"
+    }
+    // Adicione mais textos conforme necessário
+};
+// Objeto de textos em portugues
+const textosPortugues = {
+    "Quem é o 'Niño Guerrero', o líder do Tren de Aragua que controlava a prisão de Tocorón": {
+        texto: "Ele é classificado como um dos homens mais perigosos da Venezuela. Héctor Guerrero Flores, mais conhecido como o “Niño Guerrero”, é o líder do Tren de Aragua, o maior grupo de crime organizado do país e um dos mais importantes da América Latina. Seu centro de operações era a prisão de Tocorón, localizada a cerca de 140 quilômetros a sudoeste de Caracas, que foi intervencionada por milhares de policiais e militares nesta quarta-feira para desmantelar as gangues criminosas. As autoridades venezuelanas afirmaram que conseguiram retomar o controle total da prisão; no entanto, não especificaram o que aconteceu com os líderes da organização criminosa.",
+        imagem: "https://ichef.bbci.co.uk/news/800/cpsprodpb/7339/live/91d730e0-5932-11ee-86c3-87504a353e41.jpg"
+    },
+    "Como reconfigurei meu cérebro em 6 semanas": {
+        texto: "Há cada vez mais evidências de que mudanças simples e cotidianas em nossas vidas podem alterar nosso cérebro e mudar seu funcionamento. Melissa Hogenboom, jornalista da BBC, submeteu seu próprio cérebro a uma varredura para descobrir. 'É surpreendentemente difícil não pensar em nada', penso enquanto estou deitada dentro da boca de uma máquina de varredura cerebral. Mandam-me concentrar-me numa cruz preta enquanto a máquina de ressonância magnética funcional (fMRI) faz o seu trabalho ruidoso. Também me é impossível manter os olhos abertos. O zumbido do scanner é algo hipnótico e estou um pouco preocupada em adormecer e como isso pode afetar a aparência do meu cérebro nas imagens resultantes.",
+        imagem: "https://ichef.bbci.co.uk/news/800/cpsprodpb/3942/live/00fd73c0-5704-11ee-a657-3d3f9f088e1b.jpg"
+    },
+    "A emocionante luta do músico Luciano Supervielle para que sua filha com acondroplasia receba medicação no Uruguai": {
+        texto: "Giras internacionais, concertos na Europa, projetos solo: a carreira do músico uruguaio Luciano Supervielle estava em ascensão quando sua esposa foi diagnosticada com câncer. E tudo mudou. Supervielle interrompeu sua vida profissional para priorizar a saúde de Eloísa, cuja morte dois anos depois, em 2020, ele ainda lembra comovido, e cuidar de seus dois filhos que vivem com ele em Montevidéu. Isso envolveu desafios difíceis: Julián, 11 anos, tem síndrome de Down, e Nina, 9 anos, tem acondroplasia.",
+        imagem: "https://www.revolvermag.com/sites/default/files/media/section-media/marioduplantier_credit_miikka_skaffarifilmmagic.jpg"
+    },
+    "A professora que viaja 200 quilômetros de carona todos os dias para ensinar duas crianças no meio do campo no Uruguai": {
+        texto: "Na beira da estrada, onde começa a rota 56, com seu jaleco branco para que saibam que ela é professora, ela estende o braço direito e mostra a mão. São 8 horas de uma manhã gelada de inverno, e María Domínguez (29 anos) está na entrada da pequena cidade de Florida, 90 quilômetros ao norte de Montevidéu, tentando conseguir que um motorista pare e lhe dê uma carona. Ela precisa estar na escola rural de Paso de la Cruz del Yí, a 108 quilômetros de sua casa, no meio do nada, para dar aulas a Juliana, de 4 anos, e a Benjamín, de 9 anos, os únicos dois alunos daquele centro educacional uruguaio.",
+        imagem: "https://www.noticias24hrs.com.ve/wp-content/uploads/2023/09/Screenshot_20230913-222243.jpg"
+    }
+    // Adicione mais textos conforme necessário
+};
+// Variável para armazenar o idioma atual
+let idiomaAtual = textosEspanhol;
 
-// Função para escolher um texto aleatório em espanhol
-function gerarTextoEspanholAleatorio() {
-    const chaves = Object.keys(textosEspanhol);
-    const textoAleatorio = textosEspanhol[chaves[Math.floor(Math.random() * chaves.length)]];
+// Função para escolher um texto aleatório em um idioma específico
+function gerarTextoAleatorio(idioma) {
+    const chaves = Object.keys(idioma);
+    const textoAleatorio = idioma[chaves[Math.floor(Math.random() * chaves.length)]];
     return textoAleatorio;
 }
 
-// Função para atualizar o conteúdo das tags h2 e p com texto em espanhol
 function atualizarTextoAleatorio() {
     const titulo = document.getElementById("titulo");
+    const imagem = document.getElementById("imagemNoticia");
     const texto = document.getElementById("texto");
-    const textoAleatorio = gerarTextoEspanholAleatorio();
+    const textoAleatorio = gerarTextoAleatorio(idiomaAtual);
 
-    titulo.textContent = Object.keys(textosEspanhol).find(key => textosEspanhol[key] === textoAleatorio);
-    texto.textContent = textoAleatorio;
+    titulo.textContent = Object.keys(idiomaAtual).find(key => idiomaAtual[key] === textoAleatorio);
+    imagem.src = textoAleatorio.imagem;
+    texto.textContent = textoAleatorio.texto;
 }
+
+// Função para trocar o idioma para espanhol
+function trocarParaEspanhol() {
+    idiomaAtual = textosEspanhol;
+    atualizarTextoAleatorio();
+}
+
+// Função para trocar o idioma para inglês
+function trocarParaIngles() {
+    idiomaAtual = textosIngles;
+    atualizarTextoAleatorio();
+}
+
+// Função para trocar o idioma para português
+function trocarParaPortugues() {
+    idiomaAtual = textosPortugues;
+    atualizarTextoAleatorio();
+}
+
+// Adicione event listeners para os links de idioma
+document.querySelector("#linkIngles").addEventListener("click", function (event) {
+    event.preventDefault(); // Evita que o link redirecione
+    trocarParaIngles(); // Chama a função para trocar para inglês
+});
+
+document.querySelector("#linkPortugues").addEventListener("click", function (event) {
+    event.preventDefault(); // Evita que o link redirecione
+    trocarParaPortugues(); // Chama a função para trocar para português
+});
+
+// Adicione event listeners para os links de idioma
+document.querySelector("#linkInglesCelular").addEventListener("click", function (event) {
+    event.preventDefault(); // Evita que o link redirecione
+    trocarParaIngles(); // Chama a função para trocar para inglês
+});
+
+document.querySelector("#linkPortuguesCelular").addEventListener("click", function (event) {
+    event.preventDefault(); // Evita que o link redirecione
+    trocarParaPortugues(); // Chama a função para trocar para português
+});
+
+// Chama a função para exibir um texto aleatório inicialmente
+document.addEventListener("DOMContentLoaded", function () {
+    atualizarTextoAleatorio();
+});
+
 let reproduzindo = false;
 // Variáveis para gravar áudio
 let mediaRecorder;
@@ -74,14 +175,20 @@ function pararGravacaoEReproduzir() {
         console.log("Reprodução de voz do usuário pausada");
     }
 }
-
-// Função para ouvir o texto gerado em espanhol
-function ouvirTexto() {
+// Função para ouvir o texto no idioma correspondente
+function ouvirTexto(idioma) {
     const texto = document.getElementById("texto").textContent;
     const utterance = new SpeechSynthesisUtterance(texto);
 
-    // Define a voz em espanhol (espanhol da Espanha)
-    utterance.voice = window.speechSynthesis.getVoices().find(voice => voice.lang === "es-ES");
+    // Define a voz com base no idioma
+    if (idioma === "espanhol") {
+        utterance.voice = window.speechSynthesis.getVoices().find(voice => voice.lang === "es-ES");
+    } else if (idioma === "ingles") {
+        utterance.voice = window.speechSynthesis.getVoices().find(voice => voice.lang === "en-US");
+    } else if (idioma === "portugues") {
+        // Defina aqui a voz em português adequada (exemplo: "pt-BR")
+        utterance.voice = window.speechSynthesis.getVoices().find(voice => voice.lang === "pt-BR");
+    }
 
     if (reproduzindo) {
         speechSynthesis.pause();
@@ -94,9 +201,7 @@ function ouvirTexto() {
     }
 }
 
-// ...
-
-// Adicione um event listener para quando a reprodução de áudio terminar
+// Adicionando um event listener para quando a reprodução de áudio terminar
 if (audio) {
     audio.addEventListener("ended", function () {
         reproduzindo = false;
